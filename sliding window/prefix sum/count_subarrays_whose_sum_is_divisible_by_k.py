15de843eba1ee4e9dc888d50cc8ff0d36c364a56
charansai1432 <charansaichintala890@gmail.com>
2026-04-06 17:47:27 +0530

    solved the problem divisible by k

--- sliding window/prefix sum/count_subarrays_whose_sum_is_divisible_by_k.py
+++ sliding window/prefix sum/count_subarrays_whose_sum_is_divisible_by_k.py
@@ -11,7 +11,7 @@ def count_subarrays_whose_sum_is_divisible_by_k(arr,k):
         
         rem =  cur_sum%k
         
-        if rem < 0:
+        if rem < 0:             #To handle the negative values i.e the remainder can also be negative in such case this condition is used 
             rem += k
         
         if rem in freq:
@@ -22,3 +22,30 @@ def count_subarrays_whose_sum_is_divisible_by_k(arr,k):
     return count
 
 print(count_subarrays_whose_sum_is_divisible_by_k([4, 5, 0, -2, -3, 1],5))          #7
+
+
+
+# What does rem += k do?
+
+# If:
+
+# rem = -2
+# k = 5
+
+# Then:
+
+# rem += k  →  -2 + 5 = 3
+
+# 👉 It converts a negative remainder into correct positive remainder
+
+# 🔥 Why is this necessary?
+
+# Because we use rem as a key in hashmap
+
+# If you don’t fix it:
+
+# One time remainder = 3
+# Another time remainder = -2
+
+# 👉 These should be treated as SAME (mathematically)
+# 👉 But hashmap treats them as DIFFERENT ❌
