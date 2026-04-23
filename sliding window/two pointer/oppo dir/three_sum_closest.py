73162dde013f51c0bcc7dd527ad7d4ca37390347
charansai1432 <charansaichintala890@gmail.com>
2026-04-23 21:58:20 +0530

    solved the three sum closest question

--- sliding window/two pointer/oppo dir/three_sum_closest.py
+++ sliding window/two pointer/oppo dir/three_sum_closest.py
@@ -8,3 +8,45 @@
 # Explanation:
 # Closest sum is (-1 + 2 + 1 = 2)
 
+# brute force approach 
+
+def three_sum_closest(arr,target):
+    arr.sort()
+    n = len(arr)
+    closest = float('inf')
+    for i in range(n):
+        for j in range(i+1,n):
+            for k in range(j+1,n):
+                total = arr[i] + arr[j] + arr[k]
+                
+                if abs(total - target) < abs(total - closest):
+                    closest = total 
+    return closest
+print(three_sum_closest([-1,2,1,-1],1))             #2
+
+# optimal solution approach 
+
+def three_sum_closest(arr,target):
+    n = len(arr)
+    closest = float('inf')
+    arr.sort()
+    
+    for i in range(n-2):
+        l = i + 1
+        r = n - 1
+        total = arr[i]+arr[l]+arr[r]
+        if abs(target - total) < abs(target - closest):
+            closest = total
+            
+        if total < target:
+            l+=1
+            
+        elif total > target:
+            r -= 1
+            
+        else:
+            return total            # when exact match i.e when total = closest i think not exactly
+            
+    return closest
+
+print(three_sum_closest([-1,2,1,-4],1))             #2
