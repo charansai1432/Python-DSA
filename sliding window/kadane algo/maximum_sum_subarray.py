0f3163eb6a348631f7185f8e05b0d9db05510e1a
charansai1432 <charansaichintala890@gmail.com>
2026-05-03 21:02:37 +0530

    modified some code in this files

--- sliding window/kadane algo/maximum_sum_subarray.py
+++ sliding window/kadane algo/maximum_sum_subarray.py
@@ -17,13 +17,14 @@ def maximum_sum_subarray(arr):
     max_sum = -1
     cur_sum = 0
     for i in range(len(arr)):
+        cur_sum = 0
         for j in range(i,len(arr)):
-            cur_sum = sum(arr[i:j+1])
+            cur_sum += arr[j]
             # cur_sum = max(arr[i],cur_sum+arr[i])
             max_sum = max(cur_sum,max_sum)
             
     return max_sum
-print(maximum_sum_subarray([-2,1,-3,4,-1,2,1,-5,4]))  
+print(maximum_sum_subarray([-2,1,-3,4,-1,2,1,-5,4]))  #17
 
 
 #optimal solution 
@@ -36,4 +37,4 @@ def maximum_sum_subarray(arr):
         cur_sum = max(i,i+cur_sum)          #kadane algo 
         max_sum = max(max_sum,cur_sum)
     return max_sum
-print(maximum_sum_subarray([-2,1,-3,4,-1,2,1,-5,4]))
+print(maximum_sum_subarray([-2,1,-3,4,-1,2,1,-5,4])) #6
