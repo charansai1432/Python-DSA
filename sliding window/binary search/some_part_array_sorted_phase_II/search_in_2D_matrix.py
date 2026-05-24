13069f16223cc2915a44c38d7b94917b1dd1005f
charansai1432 <charansaichintala890@gmail.com>
2026-05-24 12:31:21 +0530

    solved the 2d matrix question using the brute force approach

--- sliding window/binary search/some_part_array_sorted_phase_II/search_in_2D_matrix.py
+++ sliding window/binary search/some_part_array_sorted_phase_II/search_in_2D_matrix.py
@@ -3,5 +3,37 @@
 # brute force apporach 
 
 def search_in_2D_matrix(matrix,target):
+    for row in matrix:
+        for num in row: 
+            if num == target:
+                return True
+    return False
+print(search_in_2D_matrix([
+ [1,3,5,7],
+ [10,11,16,20],
+ [23,30,34,60]
+],11))
+print(search_in_2D_matrix([
+ [1,3,5,7],
+ [10,11,16,20],
+ [23,30,34,60]
+],0))
+print(search_in_2D_matrix([
+ [],
+ [],
+ []
+],0))
+
+
+# optimal solution approach using the binary search
+
+def search_in_2D_matirx(arr,target):
+    
+    
     pass
-print(search_in_2D_matrix())
+    
+print(search_in_2D_matrix([
+ [1,3,5,7],
+ [10,11,16,20],
+ [23,30,34,60]
+],11))
