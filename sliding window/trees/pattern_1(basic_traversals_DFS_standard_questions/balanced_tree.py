e58f2d8e8ecfe6f25aa2038cbdcb3942e262bb53
charansai1432 <charansaichintala890@gmail.com>
2026-07-08 23:37:20 +0530

    solved the diameter and balanced tree problems

---
+++ sliding window/trees/pattern_1(basic_traversals_DFS_standard_questions/balanced_tree.py
@@ -0,0 +1,42 @@
+
+
+# optimal solution 
+
+class Treenode:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+        
+def balanced_tree(root):
+    if root is None:
+        return 0
+    l_height = balanced_tree(root.left)
+    if l_height == -1:
+        return -1
+    r_height = balanced_tree(root.right)
+    if r_height == -1:
+        return -1
+    if abs(l_height - r_height) > 1:
+        return -1
+    # FIX: Return the actual height of this subtree to the parent
+    return 1 + max(l_height, r_height)
+    # if abs(l_height - r_height) == 1:
+    #     return True 
+    # return False
+root = Treenode(1)
+root.left = Treenode(2)
+root.right = Treenode(3)
+root.left.left = Treenode(4)
+root.left.right = Treenode(5)
+balanced = balanced_tree(root) != -1
+print(balanced)
+
+
+
+# To fix this without writing two separate, slow functions, we make one single integer do two jobs:
+
+# Job 1 (Height): If a subtree is balanced, it returns its actual height (e.g., 1, 2, 3) using 1 + max(l_height, r_height). T
+# he parent uses this number to check its own balance.
+
+# Job 2 (The Flag): If a subtree is unbalanced, it returns -1. The moment a parent sees -1, it stops calculating and just passes -1 all the way to the top.
---
+++ sliding window/trees/pattern_1(basic_traversals_DFS_standard_questions/diameter_of_binary_tree.py
@@ -0,0 +1,58 @@
+
+# optimal solution 
+
+class Treenode:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+root = Treenode(1)
+root.left = Treenode(2)
+root.right = Treenode(3)
+root.left.left = Treenode(4)
+root.left.right= Treenode(5)
+
+
+def diameter_of_tree(root):
+    diameter = 0 
+    def height_of_left_right_tree(root):
+        nonlocal diameter
+        if root is None:
+            return 0
+        l_height = height_of_left_right_tree(root.left)
+        r_height = height_of_left_right_tree(root.right)
+        diameter = max(diameter,l_height+r_height)
+        return 1 + max(l_height,r_height)
+    height_of_left_right_tree(root)
+    return diameter
+
+
+
+
+
+print(diameter_of_tree(root))
+# here the inside function does 2 things 
+    # 1. update the global answer 
+    # 2. return the something for the parent 
+    
+    
+# Return values are for the parent. 
+# Global variables are for the final answer.
+
+
+# Pattern Recognition
+
+# When interviewer asks:
+
+# Diameter
+# Maximum path
+# Best answer in tree
+# Global maximum
+
+# Think:
+
+# Return information
+
+# +
+
+# Maintain global answer
