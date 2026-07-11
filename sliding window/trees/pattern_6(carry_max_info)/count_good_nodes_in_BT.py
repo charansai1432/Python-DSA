37394a1d6664210d08cd3804a1ab91ed2786725a
charansai1432 <charansaichintala890@gmail.com>
2026-07-11 20:59:14 +0530

    prepared the .md file for the trees

--- sliding window/trees/pattern_6(carry_max_info)/count_good_nodes_in_BT.py
+++ sliding window/trees/pattern_6(carry_max_info)/count_good_nodes_in_BT.py
@@ -33,6 +33,15 @@ root.right.right = Treenode(5)
 
 print(good_nodes(root))             #4
 
+
+# in any tree question -> 1st in the question confirm which recrusion we have to choose like top-down or bottom-up recrusion 
+# after that in every question 1st write the base condition 
+# A/c to whcih dfs traversal (pre-order / post - order / in - order)  the question require --> process the cur_node or left or right
+# process the leaf nodes 
+
+# after processing the leaf-nodes only write the recrusive logic for the left and right sub-trees
+
+#  return the final answer according to the question 
     
     
     
---
+++ sliding window/trees/trees_technique.md
@@ -0,0 +1,15 @@
+
+# tress questions approach -> how to solve a trees questions
+
+
+# in any tree question -> 1st in the question confirm which recrusion we have to choose like top-down or bottom-up recrusion 
+# after that in every question 1st write the base condition 
+# A/c to whcih dfs traversal (pre-order / post - order / in - order)  the question require --> process the cur_node or left or right
+# process the leaf nodes 
+
+# after processing the leaf-nodes only write the recrusive logic for the left and right sub-trees
+
+#  return the final answer according to the question 
+    
+    
+    
